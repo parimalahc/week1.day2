9796ee336724b923c020df758d6d738b0500b8fa
parimalahc <42540345+parimalahc@users.noreply.github.com>
2022-02-03 18:13:27 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -5,7 +5,7 @@ public class Factorial {
 	public static void main(String[] args) {
 		int input = 5;
 		int fact = 1;
-		for (int i = 1; i <=input; i++) {
+		for (int i = input; i>0; i--) {
 			fact=fact*i;
 		}
 		System.out.println(fact);
